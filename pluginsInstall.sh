0d25e4e44ebf0c0cd58a4ed11e56edd6233c540c
Nicolas Yuste <info@nicoyuste.es>
2013-09-16 16:57:39 +0200

    Added .sh to install all the plugins to xCode at ones.

---
+++ pluginsInstall.sh
@@ -0,0 +1,4 @@
+xcrun xcodebuild -project ./xCode_Plugins/XcodeColors/XcodeColors.xcodeproj -target XcodeColors  -configuration Release
+rm -rf build
+rm -rf XcodeColors
+echo **XcodeColors installed**
