7d3988499ced9c7ad0b645129e486da253ede805
Nicolas Yuste <info@nicoyuste.es>
2013-09-16 16:17:31 +0200

    git files added.

---
+++ gitInstall.sh
@@ -0,0 +1,10 @@
+
+chmod 777 ./gitConfiguration/.gitconfig
+chmod 777 ./gitConfiguration/.gitignore_global
+chmod 777 ~/.gitconfig
+chmod 777 ~/.gitignore_global
+
+cp ./gitConfiguration/.gitignore_global ~/
+echo GIT IGONORE COPIED
+cp ./gitConfiguration/.gitconfig ~/
+echo GIT CONFIG COPIED
