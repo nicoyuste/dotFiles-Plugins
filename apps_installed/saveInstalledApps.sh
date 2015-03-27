05441c300b05db7c9f4148ddac916124dc771e8c
Nicolas Yuste <info@nicoyuste.es>
2015-03-26 22:12:08 -0700

    Brew and Gems saver and installer created

---
+++ apps_installed/saveInstalledApps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export BREW_FILE='brew_installed.txt'
+export GEMS_FILE='gems_installed.txt'
+
+echo 'Saving brew Apps...'
+brew list > $BREW_FILE
+
+echo 'Saving gems installed...'
+gem list > $GEMS_FILE
+
+export TEMP_GEMS='temp.gems.text'
+
+gem list > $TEMP_GEMS
+rm $GEMS_FILE
+touch $GEMS_FILE
+
+while read p; do
+  	
+  	gem=${p% (*}
+  	echo $gem >> $GEMS_FILE
+
+done < $TEMP_GEMS
+
+rm $TEMP_GEMS
+
