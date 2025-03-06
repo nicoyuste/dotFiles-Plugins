663209a4156c340a8e53c6c8daeca25c28f2235e
Nicolas Yuste Tirados <nicolasy@spotify.com>
2025-03-06 16:37:18 +0100

    Saving machine

--- git_configuration/install_git_configuration.sh
+++ git_configuration/install_git_configuration.sh
@@ -7,17 +7,17 @@ chmod 777 ./git-completion.bash
 chmod 777 ~/.gitconfig
 chmod 777 ~/.gitignore_global
 
-cp ./gitignore_global ~/.gitignore_global
-echo 'git global ignored installed...'
+# cp ./gitignore_global ~/.gitignore_global
+# echo 'git global ignored installed...'
 
-cp ./gitconfig ~/.gitconfig
-echo 'git config file installed...'
+# cp ./gitconfig ~/.gitconfig
+# echo 'git config file installed...'
 
-cp ./gitconfig-play ~/.gitconfig-play
-echo 'git config-play file installed...'
+# cp ./gitconfig-play ~/.gitconfig-play
+# echo 'git config-play file installed...'
 
-cp ./gitconfig-work ~/.gitconfig-work
-echo 'git config-work file installed...'
+# cp ./gitconfig-work ~/.gitconfig-work
+# echo 'git config-work file installed...'
 
 cp git-completion.bash ~/.git-completion.bash
 echo 'git-completion.bash installed...'
--- install_machine.sh
+++ install_machine.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+# if [ "$EUID" -ne 0 ]
+#   then echo "Please run as root"
+#   exit
+# fi
 
 export APPS_INSTALL_FILE='./apps_installed/install_apps.sh'
 export BASH_INSTALL_FILE='./bash_configuration/install_bash_configuration.sh'
@@ -17,22 +17,22 @@ chmod 777 $BASH_INSTALL_FILE
 chmod 777 $GIT_INSTALL_FILE
 
 # Installing Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #Installing SDK Man
-curl -s "https://get.sdkman.io" | bash
+# curl -s "https://get.sdkman.io" | bash
 
-echo
-echo '--- BREW and GEMS ---'
-cd apps_installed
-./install_apps.sh
-cd ..
+# echo
+# echo '--- BREW and GEMS ---'
+# cd apps_installed
+# ./install_apps.sh
+# cd ..
 
-echo
-echo '--- BASH CONFIGURATION ---'
-cd bash_configuration
-./install_bash_configuration.sh
-cd ..
+# echo
+# echo '--- BASH CONFIGURATION ---'
+# cd bash_configuration
+# ./install_bash_configuration.sh
+# cd ..
 
 echo
 echo '--- GIT CONFIGURATION ---'
